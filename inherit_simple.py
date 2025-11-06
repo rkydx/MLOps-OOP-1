95f917e8ab1f3ba4b4e44f157fc55e37cc154912
Ramakant <ramakant.c@gmail.com>
2025-11-06 22:39:23 +0800

    Added inheritance file

---
+++ inherit_simple.py
@@ -0,0 +1,23 @@
+# Simple inheritance
+
+# # Base class
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        print(f"{self.name} makes a sound.")
+
+# # Derived class
+class Dog(Animal):
+
+    def speak(self):
+        print(f"{self.name} barks.")
+
+# # Create an instance of Animal
+animal = Animal("Generic Animal")
+animal.speak()  # Output: Generic Animal makes a sound.
+
+# # # Create an instance of Dog
+dog = Dog('Buddy')
+dog.speak()  # Output: Buddy barks.         # it will override the speak method of base class if the derived class has the same method name
