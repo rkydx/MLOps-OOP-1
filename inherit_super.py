f46aaeac1accaebbf71e9dde246288b312f46ed2
Ramakant <ramakant.c@gmail.com>
2025-11-07 10:26:26 +0800

    Added super function in derived class

--- inherit_super.py
+++ inherit_super.py
@@ -1,6 +1,6 @@
 # Simple inheritance
 
-# # Base class
+# # Base class (parent class)
 class Animal:
     def __init__(self):
         self.name = "Buddy"
@@ -15,10 +15,11 @@ class Dog(Animal):
         self.breed = breed
 
     def speak(self):
+        super().speak()                                          # Call the speak method of the base class (ie parent class)
         print(f"{self.name} barks. It is a {self.breed}.")
 
 
 # # # Create an instance of Dog
 #dog = Dog('Buddy')
 dog = Dog("Golden Retriever")
-dog.speak()  # Output: Buddy barks. It is a Golden Retriever.         # it will override the speak method of base class if the derived class has the same method name
+dog.speak()  # Output: Buddy barks. It is a Golden Retriever.    # it will override the speak method of base class if the derived class has the same method name
